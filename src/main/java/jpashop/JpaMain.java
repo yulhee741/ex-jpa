5d59a432296df96084cec53cc9e0efa6b1e16169
yulhee741 <dbfgml980413@gmail.com>
2021-10-24 22:38:07 +0900

    연관관계 매핑 시작

--- src/main/java/jpashop/JpaMain.java
+++ src/main/java/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpashop;
 
+import jpashop.domain.Order;
+import jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -15,6 +18,9 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
